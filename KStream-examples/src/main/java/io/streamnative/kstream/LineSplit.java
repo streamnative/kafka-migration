86ec2fa883344be56a549d09bb4b00eade22e6f8
david-streamlio <dkjerrumgaard@gmail.com>
2022-05-03 14:46:17 -0700

    Added Stream examples

---
+++ KStream-examples/src/main/java/io/streamnative/kstream/LineSplit.java
@@ -0,0 +1,54 @@
+package io.streamnative.kstream;
+
+import io.streamnative.util.PropertyLoader;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class LineSplit {
+
+    public static void main(final String[] args) throws Exception {
+
+        final Properties props = PropertyLoader.loadConfig(args[0]);
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "line-split-app");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("persistent://public/default/sentence-topic");
+        source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .peek((k, v) -> { System.out.println(" Value: " + v); })
+                .to("persistent://public/default/streams-linesplit-output");
+
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
---
+++ KStream-examples/src/main/java/io/streamnative/kstream/Pipe.java
@@ -0,0 +1,53 @@
+package io.streamnative.kstream;
+
+import io.streamnative.util.PropertyLoader;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class Pipe {
+
+    public static void main(final String[] args) throws Exception {
+
+        final Properties props = PropertyLoader.loadConfig(args[0]);
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "pipe-app");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        builder.stream("persistent://public/default/sentence-topic")
+                .peek((k, v) -> { System.out.println(v); })
+                .to("persistent://public/default/sentence-topic-out");
+
+        final Topology topology = builder.build();
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+
+}
